a76cbbffbfe7c5a590851739dceb15b115a46ced
James <jamesteoh0104@gmail.com>
2020-01-02 21:40:59 +0800

    Minor Changes

--- src/components/dynamicLineChart.js
+++ src/components/dynamicLineChart.js
@@ -5,18 +5,12 @@ import { db } from './firebase';
 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-// var dps = [{x: 1, y: 10}, {x: 2, y: 13}, {x: 3, y: 18}, {x: 4, y: 20}, {x: 5, y: 17},{x: 6, y: 10}, {x: 7, y: 13}, {x: 8, y: 18}, {x: 9, y: 20}, {x: 10, y: 17}];   //dataPoints.
 var dps = []
-// var xVal = dps.length + 1;
-// var yVal = 15;
-// var updateInterval = 1000;
 
 db.collection('temp_sensor_test').orderBy('timestamp','desc').limit(20).get().then(query => {
 	query.forEach(doc => {
 		let timestamp = doc.data().timestamp
 		timestamp = new Date(timestamp * 1000);
-		// timestamp = timestamp.toUTCString(); 
-		// timestamp = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp)		
 		// console.log(timestamp)
 		// console.log(typeof timestamp)
 		let temperature = doc.data().temp
@@ -37,7 +31,6 @@ class DynamicLineChart extends Component {
 			changes.forEach(change => {
                 // this.setState({current: change.doc.data().temp})
                 let timestamp = change.doc.data().timestamp;
-				// let datetime = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp);
 				timestamp = new Date(timestamp * 1000);
                 let temperature = change.doc.data().temp
 				let tempdata = {x: timestamp, y: temperature}
@@ -45,8 +38,6 @@ class DynamicLineChart extends Component {
 				this.updateChart()
 			})
 		})
-
-		// setInterval(this.updateChart, updateInterval);
 	}
 	updateChart() {
 		// yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
