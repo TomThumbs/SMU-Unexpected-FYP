4f6a25cc52a9e102f43105ce335365c6faf6fd0a
wilsonng70 <43181129+wilsonng70@users.noreply.github.com>
2020-02-03 00:27:09 +0800

    Security update

--- remind_user_sms WA tele.py
+++ remind_user_sms WA tele.py
@@ -1,19 +1,17 @@
 from twilio.rest import Client
-account_sid = 'ACe2b224e4761970ae4dbccaaf01f849b4'
-auth_token = '1d3d80432f711600a18cf5c7f239abf2'
 client = Client(account_sid, auth_token)
 
 #sms
 message = client.messages \
                 .create(
                      body=text_body,
-                     from_='+18022424685',
-                     to=hp  '+6598398720'              )
+                     from_='+1',
+                     to=hp  '+65'              )
 
 #whatsapp
 message = client.messages.create(
                               body=text_body,
-                              from_='whatsapp:+14155238886',
+                              from_='whatsapp:+1',
                               to='whatsapp:'+hp
                           )
 
