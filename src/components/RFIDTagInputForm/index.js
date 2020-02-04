15c25363b033ae05fdbc550322eb77c95e628125
James <jamesteoh0104@gmail.com>
2020-02-04 14:12:29 +0800

    Minor Changes
    Edited minor bugs

--- src/components/RFIDTagInputForm/index.js
+++ src/components/RFIDTagInputForm/index.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import '../../App.css';
 import { Link, withRouter } from 'react-router-dom';
 import { withFirebase } from '../Firebase';
+
+import 'date-fns'; //npm i date-fns
+import DateFnsUtils from '@date-io/date-fns'; //npm i @date-io/date-fns@1.x date-fns
+
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
 // import FormLabel from '@material-ui/core/FormLabel';
-import 'date-fns'; //npm i date-fns
-import DateFnsUtils from '@date-io/date-fns'; //npm i @date-io/date-fns@1.x date-fns
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
@@ -25,6 +27,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles(theme => ({
   formControl: {
@@ -119,10 +122,11 @@ class RFIDTagInputFormBase extends Component {
       let dd = String(temp_date.getDate()).padStart(2, '0');
       let mm = String(temp_date.getMonth() + 1).padStart(2, '0'); 
       let yyyy = temp_date.getFullYear();
+      let string = dd + '/' + mm + '/' + yyyy
     
       this.setState({
-        today: mm + '/' + dd + '/' + yyyy,
-        selectedDate: mm + '/' + dd + '/' + yyyy
+        today: string,
+        selectedDate: string
       })
     }
   }
@@ -132,14 +136,24 @@ class RFIDTagInputFormBase extends Component {
   }
 
   renderSubmit() {
-    if (this.state.today == this.state.selectedDate || this.state.foodType.length == 0 || this.state.name.length == 0) {
+    // if (this.state.today == this.state.selectedDate || this.state.foodType.length == 0 || this.state.name.length == 0) {
+    //   return <h1>Please select a tag, input ingredient name and choose a future expiry date</h1>
+    //       // <form onSubmit={this.onSubmit}>
+    //       //    <button type='submit'>Submit</button>
+    //       //    </form>
+    // } else {
+    //   return <form onSubmit={this.onSubmit}>
+    //          <button type='submit'>Submit</button>
+    //          </form>
+    // }
+    if (this.state.today == this.state.selectedDate || this.state.name.length == 0) {
       return <h1>Please select a tag, input ingredient name and choose a future expiry date</h1>
           // <form onSubmit={this.onSubmit}>
           //    <button type='submit'>Submit</button>
           //    </form>
     } else {
       return <form onSubmit={this.onSubmit}>
-             <button type='submit'>Submit</button>
+              <button type='submit'>Submit</button>
              </form>
     }
   }
@@ -159,18 +173,25 @@ class RFIDTagInputFormBase extends Component {
 
   render() {
     // {this.testurl()}
+
+    const isInvalid = this.state.today == this.state.selectedDate || this.state.name.length == 0;
+
     return (  
     <div>
 
       <React.Fragment>
-        <Typography variant="h6" gutterBottom>
-          RFID Tag Registration
+        <Typography align="center" variant="h6" gutterBottom>
+          Barcode Tag Registration
         </Typography>
         
 
-        <Grid container spacing={3}>
-        <Grid item xs={12}>
-        {this.state.foodId}
+        <Grid container justify="center" spacing={3} alignItems="center">
+          <Grid item xs={3}>
+          Item ID: 
+          </Grid>
+          <Grid item xs={3}>
+          <Paper>{this.state.foodId}</Paper>
+          </Grid>
         </Grid>
         {/* <Grid item xs={12}>
         <FormControl className={this.classes.formControl}>
@@ -191,8 +212,9 @@ class RFIDTagInputFormBase extends Component {
         </FormControl> */}
         {/* {this.handleRFID()}{this.state.RFID} */}
         {/* </Grid> */}
-
-          <Grid item xs={12}>
+        <Grid container justify="center" spacing={3} alignItems="center">
+          <Grid item xs={3}>Name:</Grid>
+          <Grid item xs={3}>
             <TextField
               required
               id="name" 
@@ -203,16 +225,21 @@ class RFIDTagInputFormBase extends Component {
               onChange={this.handleIngredientName('name')}
             />
           </Grid>
-          <Grid item xs={12}>
-            date of entry:  {this.today()} {this.state.today}
+        </Grid>
+        <Grid container justify="center" spacing={3} alignItems="center">
+          <Grid item xs={3}>Date of entry:</Grid>
+          <Grid item xs={3}>
+            {this.today()} {this.state.today}
           </Grid>
+        </Grid>
+        <Grid container justify="center" spacing={3} alignItems="center">
+          <Grid item xs={3}>Date of Expiry: </Grid>
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <Grid item xs={12}> {/* container justify="space-around"> */}
-          date of expiry:
+          <Grid item xs={3}> {/* container justify="space-around"> */}
             <KeyboardDatePicker
               disableToolbar
               variant="inline"
-              format="MM/dd/yyyy"
+              format="dd/MM/yyyy"
               margin="normal"
               id="date-picker-inline"
               value={this.state.selectedDate}
@@ -224,8 +251,10 @@ class RFIDTagInputFormBase extends Component {
             </Grid>
           </MuiPickersUtilsProvider>
         </Grid>
+        <Grid container justify="center" spacing={3} alignItems="center">
+          {this.renderSubmit()}
+        </Grid>
       </React.Fragment>
-      {this.renderSubmit()}
 
       <Dialog
         open={this.state.open}
