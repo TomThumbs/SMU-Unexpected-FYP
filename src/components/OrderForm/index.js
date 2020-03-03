d93358cef7ab895baa7303739f7aab6487ae7e1c
Dion <Dion95hc@gmail.com>
2020-03-03 21:49:59 +0800

    Added payment method, fixed error state when date is deleted

--- src/components/OrderForm/index.js
+++ src/components/OrderForm/index.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
-
 import "../../App.css";
-
 import { withRouter } from "react-router-dom";
 import { withFirebase } from "../Firebase";
-
 import Paper from '@material-ui/core/Paper';
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-
 import Checkbox from "@material-ui/core/Checkbox";
 import Divider from "@material-ui/core/Divider";
-
 import { withAuthorization } from "../Session";
-
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 import "date-fns";
 import DateFnsUtils from "@date-io/date-fns";
 import {
@@ -27,7 +24,6 @@ import {
 	KeyboardTimePicker,
 	KeyboardDatePicker
 } from "@material-ui/pickers";
-
 const INITIAL_STATE = {
 	orderID: 0,
 	orderiddoc: "",
@@ -47,9 +43,11 @@ const INITIAL_STATE = {
 	selectedmenu: [],
 	finalmenu: [],
 	remarks: "",
-	commencement: new Date()
+	commencement: new Date(),
+	payment: ["Cash on delivery", "Payment Made in Full"],
+	chosen_payment:"",
+	f: false
 };
-
 const useStyles = makeStyles(theme => ({
 	formControl: {
 		margin: theme.spacing(1),
@@ -64,14 +62,12 @@ const useStyles = makeStyles(theme => ({
 		color: theme.palette.text.secondary
 	}
 }));
-
 class OrderFormBase extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { ...INITIAL_STATE };
 		this.classes = { useStyles };
 	}
-
 	componentDidMount() {
 		let day = this.state.commencement.getDate();
 		let month = Number(this.state.commencement.getMonth()) + 1;
@@ -81,7 +77,7 @@ class OrderFormBase extends Component {
 		if (month.length === 1) {
 			month = "0" + month;
 		} 
-		
+
 		if (hour.length === 1) {
 			hour = "0" + hour;
 		}
@@ -92,7 +88,6 @@ class OrderFormBase extends Component {
 			commencement:
 				day + "/" + month + "/" + year + " " + hour + ":" + minute
 		});
-
 		// Detect latest order ID
 		this.props.firebase.fs
 			.collection("Catering_orders")
@@ -108,7 +103,6 @@ class OrderFormBase extends Component {
 					});
 				});
 			});
-
 		//get latest customer ID
 		this.props.firebase.fs
 			.collection("Customers")
@@ -119,7 +113,6 @@ class OrderFormBase extends Component {
 					custID: Number(docu.data().ID) + 1
 				});
 			});
-
 		// Get list of menu items
 		this.props.firebase.fs
 			.collection("Menu")
@@ -143,32 +136,8 @@ class OrderFormBase extends Component {
 			});
 	}
 
-	// onSubmit = event => {
-	// 	event.preventDefault();
-
-	// 	let finalmenu = []
-
-	// 	this.state.selectedmenu.forEach(item => {
-	// 		let dishname = item.dish + " checkbox";
-	// 		if (dishname in this.state) {
-	// 			// console.log(dishname);
-	// 			// console.log(dishname in this.state);
-	// 			if (this.state[dishname] === true) {
-	// 				// console.log(this.state[dishname]);
-	// 				// this.setState(prevstate => ({
-	// 				// 	finalmenu: [...prevstate.finalmenu, item.dish]
-	// 				// }));
-	// 				finalmenu.push(item.dish)
-	// 			}
-	// 		}
-	// 	});
-
-	// 	console.log(finalmenu)
-	// };
-
 	onSubmit = event => {
 		event.preventDefault();
-
 		let strhour = String(this.state.hour);
 		let strmonth = Number(this.state.date.getMonth()) + 1;
 		let strmin = "";
@@ -177,7 +146,6 @@ class OrderFormBase extends Component {
 		} else {
 			strmin = String(this.state.minute);
 		}
-
 		let strtime = "";
 		if (strhour.length === 1) {
 			strtime = "0" + String(this.state.hour) + strmin;
@@ -199,43 +167,27 @@ class OrderFormBase extends Component {
 			0
 		);
 		let strSubmitDate = String(submitDate);
-
-    let strMonth = Number(new Date().getMonth()) + 1;
-
-    // let uniqueMenu = Array.from(new Set(this.state.finalmenu))
-    // console.log(this.state.finalmenu, "ppppppppppppppp", Array.from(new Set(this.state.finalmenu)))
-    this.props.firebase.fs.collection("Catering_orders").add({
-      Customer: "",
-      Status: "Order Received",
-      Date: submitDate,
-      DateOnly: strDate,
-      // DeliveryCheck: false,
-      Menu: Array.from(new Set(this.state.finalmenu)),
-      Pax: Number(this.state.pax),
-      Time: strtime,
-      // TruckImgUrl: '',
-      venue: this.state.venue,
-      orderID: this.state.orderID,
-      sop: false,
-      HeatersUsed: heatersUsed,
-      StatusDates: [this.state.commencement],
-      Created_On: this.state.commencement
-  
-    });
-    
+		strSubmitDate = strSubmitDate.split("GMT")[0];
+		let finalmenu = [];
+		this.state.selectedmenu.forEach(item => {
+			let dishname = item.dish + " checkbox";
+			if (dishname in this.state) {
+				if (this.state[dishname] === true) {
+					finalmenu.push(item.dish);
+				}
+			}
+		});
 		let heatersUsed = {};
-
 		finalmenu.forEach(dish => {
 			heatersUsed = { ...heatersUsed, [dish]: "unallocated" };
 		});
-
 		// let strMonth = Number(new Date().getMonth()) + 1;
 		this.props.firebase.fs.collection("Catering_orders").add({
 			Customer: "",
 			Status: "Order Received",
 			Date: submitDate,
 			DateOnly: strDate,
-			// DeliveryCheck: false,
+			Payment: this.state.chosen_payment,
 			Menu: finalmenu,
 			Pax: Number(this.state.pax),
 			Time: strtime,
@@ -250,7 +202,6 @@ class OrderFormBase extends Component {
 		});
 
 		let notCreated = true;
-
 		this.props.firebase.fs
 			.collection("Customers")
 			.where("Email", "==", this.state.custemail)
@@ -259,13 +210,11 @@ class OrderFormBase extends Component {
 				snap.forEach(doc => {
 					if (doc.exists) {
 						this.setState({ custExists: true });
-
 						// console.log('Customer exists')
 						this.setState({ custref: "Customers/" + doc.id });
 						let dbcustref = this.props.firebase.fs.doc(
 							"Customers/" + doc.id
 						);
-
 						this.props.firebase.fs
 							.collection("Catering_orders")
 							.where("orderID", "==", this.state.orderID)
@@ -317,9 +266,7 @@ class OrderFormBase extends Component {
 					});
 				});
 		}
-
 		let nPax = Number(this.state.pax);
-
 		this.props.history.push({
 			pathname: "./post-order-form",
 			orderID: this.state.orderID,
@@ -337,39 +284,40 @@ class OrderFormBase extends Component {
 		});
 	};
 
+	handleMenuChange = event => {
+		this.setState({
+		  chosen_payment: event.target.value
+		})
+	  }
 	onMenuChange = event => {
 		// const dishname = event.target.value;
-
 		// if(this.state.finalmenu.includes(dishname) === false){
 		//   this.setState(prevstate => ({
 		//     finalmenu: [...prevstate.finalmenu, dishname]
 		//   }));
 		// }
-
 		const target = event.target;
 		const value =
 			target.type === "checkbox" ? target.checked : target.value;
 		const name = target.name;
-
 		this.setState({
 			[name + " checkbox"]: value
 		});
-
 		console.log(this.state);
-
 		// this.setState(prevstate => ({
 		// 	finalmenu: [...prevstate.finalmenu, dishname]
 		// }));
 		// console.log(event.target.name + ": " + event.target.value)
 	};
-
 	handleDateChange = event => {
-		this.setState({
-			date: event
-		});
-		// console.log(this.state.date)
-	};
 
+		if (event) {
+			this.setState({
+				date: event
+			});
+		} else {
+		}
+	};
 	handleTimeChange = time => {
 		this.setState({
 			starttime: time,
@@ -378,12 +326,12 @@ class OrderFormBase extends Component {
 		});
 		// console.log(this.state.starttime)
 	};
-
 	createTextField = (name, temp, label, placeholder) => {
 		return (
 			<TextField
 				margin="densed"
 				fullWidth
+				required
 				name={name}
 				value={temp}
 				label={label}
@@ -393,12 +341,10 @@ class OrderFormBase extends Component {
 			/>
 		);
 	};
-
 	today = new Date();
 	renderMenu = () => {
 		let listofmenu = [];
 		let dishtype = [];
-
 		this.state.selectedmenu.forEach(item => {
 			if (dishtype.includes(item.type) === false) {
 				dishtype.push(item.type);
@@ -423,12 +369,10 @@ class OrderFormBase extends Component {
 				</div>
 			);
 		});
-
 		return listofmenu;
 	};
-
 	render() {
-		let isInvalid =
+		let isInvalid = 
 			this.state.date.length !== 0 &&
 			this.state.starttime.length !== 0 &&
 			this.state.venue.length !== 0 &&
@@ -437,7 +381,6 @@ class OrderFormBase extends Component {
 			this.state.custcontact.length !== 0 &&
 			this.state.custemail.length !== 0 &&
 			this.state.custcompany.length !== 0;
-
 		return (
 			<Container component="main" maxWidth="sm">
 				<Typography variant="h4"  gutterBottom>Order Form</Typography>
@@ -460,21 +403,20 @@ class OrderFormBase extends Component {
 							<br></br>
 						</div>
 						<Grid container spacing={3}>
-						
-							<MuiPickersUtilsProvider utils={DateFnsUtils} >
+						<MuiPickersUtilsProvider utils={DateFnsUtils} >
 							<Grid item xs={6} >
 								<KeyboardDatePicker
 									variant="inline"
 									InputLabelProps={{ shrink: true }}
 									fullWidth
 									margin="densed"
-									
 									label="Date:"
 									format="dd/MM/yyyy"
 									minDate={this.today}
 									id="date-picker-inline"
 									value={this.state.date}
 									onChange={this.handleDateChange}
+									allowKeyboardControl={this.state.f}
 									KeyboardButtonProps={{
 										"aria-label": "change date"
 									}}
@@ -507,7 +449,6 @@ class OrderFormBase extends Component {
 									"Customer Name"
 								)}
 							</Grid>
-
 							<Grid item xs={6} >
 								{/* Customer Company */}
 								{this.createTextField(
@@ -547,7 +488,6 @@ class OrderFormBase extends Component {
 									"Venue"
 								)}
 							</Grid>
-
 							<Grid item xs={6} >
 							<TextField
 								margin="densedl"
@@ -564,12 +504,25 @@ class OrderFormBase extends Component {
 								inputProps={{ min: 30 }}
 							/>
 							</Grid>
-							
+							<Grid item xs={6} >
+							<FormControl style={{minWidth:250}}>
+							<InputLabel>Payment Mode:</InputLabel>
+							<Select
+						
+								value={this.state.chosen_payment}
+								onChange={this.handleMenuChange}
+								
+							>
+							{this.state.payment.map((event, index) =>
+								<MenuItem value={event}>{event}</MenuItem>
+							)}
+							</Select>
+							</FormControl>
+							{/* here */}
+							</Grid>
 							<p><Divider variant="il" /></p>
-
-							
 							<Grid item xs={12} >
-								<Typography component="h5" variant="h5">Menu</Typography>
+							<Typography component="h5" variant="h5">Menu</Typography>
 
 							{/* Display Menu */}
 							{this.renderMenu()}
@@ -605,8 +558,6 @@ class OrderFormBase extends Component {
 		);
 	}
 }
-
 const OrderForm = withRouter(withFirebase(OrderFormBase));
 const condition = authUser => !!authUser;
-
 export default withAuthorization(condition)(OrderForm);
