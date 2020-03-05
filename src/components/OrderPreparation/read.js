7384321863f80606e34169cff284658d7a330faf
James <jamesteoh0104@gmail.com>
2020-03-05 18:45:49 +0800

    Fixed Order Preparation Read Page
    ----- Order Preparation Read -----
    Added function to display ingredient information used

--- src/components/OrderPreparation/read.js
+++ src/components/OrderPreparation/read.js
@@ -8,6 +8,7 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 // import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 
 import * as ROUTES from "../../constants/routes";
@@ -51,15 +52,19 @@ const INITIAL_STATE = {
 	headchef: "",
 	assistantA: "",
 	assistantB: "",
-	kitchenImageURL:"",
-	commence:"",
-	searchId:""
+	kitchenImageURL: "",
+	commence: "",
+	searchId: "",
+	ingredients: {}
 };
 
 class OrderPreparationBase extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { ...INITIAL_STATE, orderID: props.location.state.orderID }; //props.location.state.orderID
+		this.state = {
+			...INITIAL_STATE,
+			orderID: props.location.state.orderID
+		}; //props.location.state.orderID
 		this.classes = { useStyles };
 	}
 
@@ -71,6 +76,36 @@ class OrderPreparationBase extends Component {
 		this.setState({
 			orderID: urlId
 		});
+
+		// ---------- RETRIEVE INGREDIENTS ----------
+		console.log("Retreving Menu Ingredients");
+		this.props.firebase.fs
+			.collection("Ingredients")
+			.get()
+			.then(querySnapshot => {
+				querySnapshot.forEach(doc => {
+					let data = doc.data();
+					let ingtname = data.name;
+					// ingtname = ingtname.toLowerCase();
+					// this.setState({
+					// 	// [data.name.toLowerCase()]: Number(data.barcode),
+					// 	[Number(data.barcode)]: ingtname
+					// });
+					this.setState(prevState => ({
+						ingredients: {
+							...prevState.ingredients,
+							[Number(data.barcode)]: [
+								ingtname,
+								data.Date_of_Storage,
+								data.Date_of_expiry
+							]
+						}
+					}));
+				});
+			})
+			.catch(function(error) {
+				console.log("Error getting documents: ", error);
+			});
 
 		// ---------- RETRIEVE CATERING ORDER ----------
 		this.props.firebase.fs
@@ -87,62 +122,12 @@ class OrderPreparationBase extends Component {
 						assistantB: data.assistantB,
 						kitchenImageURL: data.kitchenImageURL,
 						commence: data.preparationCommencement,
-						statusDate: data.StatusDates[1]
+						statusDate: data.StatusDates[1],
+						menu: data.Menu,
+						ingredientsUsed: data.IngredientsUsed
 					});
-					// console.log(data.IngredientsUsed)
-					//this stores ingredient IDs
-					for (var key in data.IngredientsUsed){
-
-						let splitIngredients = []
-						splitIngredients = data.IngredientsUsed[key].split(",")
-						// console.log(splitIngredients)
-						let ingreDetails = []
-						
-						let dos = ""
-						let doe = ""
-						let ingrename = ""
-						//this finds the ingredients details of each ingredient ID
-						splitIngredients.forEach((element) => { 
-							// console.log("ingre", element)
-							this.props.firebase.fs
-							.collection("Ingredients")
-							.doc(element)
-							.get()
-							.then(query => {
-								// console.log("thisdata", query.data())
-								// query.forEach(doc => {
-									let dos = query.data().Date_of_Storage,
-									doe = query.data().Date_of_expiry,
-									ingrename = query.data().name
-								// })
-								ingreDetails.push([query.data().name, element, query.data().Date_of_Storage, query.data().Date_of_expiry])
-							})
-							
-							// console.log(ingreDetails)
-						})
-						// console.log(data.IngredientsUsed[key])
-						this.setState({
-							...this.state,
-							menuIngreDict: {
-								...this.state.menuIngreDict,
-								[key]: ingreDetails
-							// 	[key]: [
-							// 		[spaghetti, 789, dos, doe],
-							// 		[spaghetti, 789, dos, doe],
-							// ]
-								// [key]: data.IngredientsUsed[key]
-							}
-						})
-
-
-
-						this.setState(
-							{ menu: [...this.state.menu, key] }
-						)
-
-					}
 				});
-			})
+			});
 	}
 
 	onChange = event => {
@@ -153,8 +138,8 @@ class OrderPreparationBase extends Component {
 	};
 
 	onSubmit = event => {
-			this.props.history.push({
-			pathname: './order-preparation-post-sop',
+		this.props.history.push({
+			pathname: "./order-preparation-post-sop",
 			search: "?id=" + this.state.orderID,
 			orderID: this.state.orderID,
 			headchef: this.state.headchef,
@@ -165,148 +150,75 @@ class OrderPreparationBase extends Component {
 		});
 	};
 
-	renderMenuItem(item, valuee) {
-		// console.log(value)
+	renderMenuItem(dish) {
+		let ingts = [];
 
-//@james got bug here
+		ingts.push(
+			<Grid item xs={3} key='Ingredient ID'>
+				Ingredient ID
+			</Grid>
+		);
+		ingts.push(
+			<Grid item xs={3} key='Ingredient Name'>
+				Ingredient Name
+			</Grid>
+		);
+		ingts.push(
+			<Grid item xs={3} key='Storage Date'>
+				Storage Date
+			</Grid>
+		);
+		ingts.push(
+			<Grid item xs={3} key='Expiry Date'>
+				Expiry Date
+			</Grid>
+		);
 
-		let menu = [];
-		if (item !== undefined) {
-			console.log("~~~~", Object.values(valuee))
+		let ingredients = this.state.ingredientsUsed[dish].split(",");
 
+		ingredients.forEach(barcode => {
+			ingts.push(
+				<Grid item xs={3} key={barcode}>
+					{barcode}
+				</Grid>
+			);
+			ingts.push(
+				<Grid item xs={3} key={barcode + " name"}>
+					{this.state.ingredients[barcode][0]}
+				</Grid>
+			);
+			ingts.push(
+				<Grid item xs={3} key={barcode + ' storage'}>
+					{this.state.ingredients[barcode][1]}
+				</Grid>
+			);
+			ingts.push(
+				<Grid item xs={3} key={barcode + 'expiry'}>
+					{this.state.ingredients[barcode][2]}
+				</Grid>
+			);
+		});
 
-			valuee.forEach((v)=> {
-				console.log("============", v[0])
-					menu.push(
-						<tr>
-							<td>{v[0]}</td>
-							<td>{v[1]}</td>
-							<td>{v[2]}</td>
-							<td>{v[3]}</td>
-						</tr>
-					)
-				})
-			
-				// menu.push(
-				// 	<tr>
-				// 		{/* ) */}
-				// {/* menu.push( */}
-				// 		<td >{item}</td>
-				// 		<td >{value}</td>
-				// 	</tr>
-
-				// );
-				// console.log(menu)
-			;
-		}
-		return menu;
+		return ingts;
 	}
 
 	// ---------- Dish Selection ----------
 	renderMenu() {
-		let list = [];
-		let altlist = [];
-		// if (this.state.chosenMenu.length === 0) {
-			list.push(
-				<div>
-				<Paper className={this.classes.paper}>
-				<Typography variant="h6" align="center" gutterBottom>
-					Preparation Details
-				</Typography>
-				<Typography variant="h6" align="center" gutterBottom>
-					Head Chef: {this.state.headchef}
-				</Typography>
-				<Typography variant="h6" align="center" gutterBottom>
-					Assistant A: {this.state.assistantA}
-				</Typography>
-				<Typography variant="h6" align="center" gutterBottom>
-					Assistant B: {this.state.assistantB}
-				</Typography>
-				<br></br>
-				<Typography variant="h6" align="center" gutterBottom>
-					Order Commence: {this.state.commence}
-				</Typography>
-				<form onSubmit={this.onSubmit}>
-				<Button
-					type="submit"
-					fullWidth
-					variant="contained"
-					color="primary"
-					className={this.classes.submit}
-				>
-					Declaration
-				</Button>
-				</form>
-				</Paper>
-			</div>
-			)
-		// }
-		console.log(this.state)
-		this.state.menu.forEach((item, id) => {
-			
-			// if (item === this.state.chosenMenu) {
-				for (var key in this.state.menuIngreDict){
-					// console.log(key,item)
-					if (key === item) {
-						altlist.push(
-							<table>
-								<tr>
-									<th>Item</th>
-									<th>Item ID</th>
-								</tr>
-								{this.renderMenuItem(key, this.state.menuIngreDict[key])}
-								
-							</table>
-						);
-					}
-				};
-				// console.log("===========")
-				list.push(
-					<div key={id}>
-						<Paper className={this.classes.paper}>
-							<Typography variant="h5">
-								{item}
-								{/* <Link name={item} onClick={this.onChange}>
-									{item}
-								</Link> */}
-							</Typography>
-						</Paper>
-					</div>
-					// push altlist here
-				);
-				list.push(altlist)
-				altlist = []
-			// } else {
-			// 	list.push(
-			// 		<div key={id}>
-			// 			<Paper className={this.classes.paper}>
-			// 				<Typography variant="h6" underline="none">
-			// 					{" "}
-			// 					<Link name={item} onClick={this.onChange}>
-			// 						{item}
-			// 					</Link>
-			// 				</Typography>
-			// 			</Paper>
-			// 		</div>
-			// 	);
-			// }
+		// console.log(this.state);
+		let menu = [];
+
+		this.state.menu.forEach(dish => {
+			menu.push(
+				<div key={dish}>
+					<Typography variant="h4" key={dish}>
+						{dish}
+					</Typography>
+					<Grid container>{this.renderMenuItem(dish)}</Grid>
+				</div>
+			);
 		});
 
-			// for (var key in this.state.menuIngreDict){
-			// 	// if (key === this.state.chosenMenu) {
-			// 		list.push(
-			// 			<table>
-			// 				<tr>
-			// 					<th>Item</th>
-			// 					<th>Item ID</th>
-			// 				</tr>
-			// 				{this.renderMenuItem(key, this.state.menuIngreDict[key])}
-			// 			</table>
-			// 		);
-			// 	// }
-			// };
-
-		return list;
+		return menu;
 	}
 
 	renderBackButton() {
@@ -325,22 +237,54 @@ class OrderPreparationBase extends Component {
 	render() {
 		// console.log(this.state);
 		return (
-				<Container component="main" maxWidth="xs" className={this.classes.root}>
-					{this.renderBackButton()}
+			<Container
+				component="main"
+				maxWidth="xs"
+				className={this.classes.root}
+			>
+				{this.renderBackButton()}
+				<Paper className={this.classes.paper}>
+					<Typography variant="h3" align="center" gutterBottom>
+						Preparation
+					</Typography>
+					<Typography variant="h4" align="center" gutterBottom>
+						Order Number: {this.state.orderID}
+					</Typography>
+					<Typography variant="h6" align="center" gutterBottom>
+						Ingredient Breakdown
+					</Typography>
 					<Paper className={this.classes.paper}>
-						<Typography variant="h3" align="center" gutterBottom>
-							Preparation
-							</Typography>
-						<Typography variant="h4" align="center" gutterBottom>
-							Order Number: {this.state.orderID}
+						<Typography variant="h6" align="center" gutterBottom>
+							Preparation Details
 						</Typography>
 						<Typography variant="h6" align="center" gutterBottom>
-							Ingredient Breakdown
+							Head Chef: {this.state.headchef}
 						</Typography>
-						{this.renderMenu()}
+						<Typography variant="h6" align="center" gutterBottom>
+							Assistant A: {this.state.assistantA}
+						</Typography>
+						<Typography variant="h6" align="center" gutterBottom>
+							Assistant B: {this.state.assistantB}
+						</Typography>
+						<br></br>
+						<Typography variant="h6" align="center" gutterBottom>
+							Order Commence: {this.state.commence}
+						</Typography>
+						<form onSubmit={this.onSubmit}>
+							<Button
+								type="submit"
+								fullWidth
+								variant="contained"
+								color="primary"
+								className={this.classes.submit}
+							>
+								Declaration
+							</Button>
+						</form>
 					</Paper>
-				</Container>
-		
+					{this.renderMenu()}
+				</Paper>
+			</Container>
 		);
 	}
 }
