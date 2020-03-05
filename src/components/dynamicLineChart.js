6512c8390d7a8cadaeefb790a2cc5403221e497d
James <jamesteoh0104@gmail.com>
2020-03-05 12:58:38 +0800

    Minor formatting changed
    Reformat for easier viewing

--- src/components/dynamicLineChart.js
+++ src/components/dynamicLineChart.js
@@ -1,51 +1,60 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-import CanvasJSReact from '../assets/canvasjs.react';
-import Typography from '@material-ui/core/Typography';
+import CanvasJSReact from "../assets/canvasjs.react";
+import Typography from "@material-ui/core/Typography";
 
-import { withFirebase } from './Firebase';
+import { withFirebase } from "./Firebase";
 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-var dps = []
+var dps = [];
 
 class DynamicLineChartBase extends Component {
 	constructor(props) {
 		super(props);
 		// this.updateChart = this.updateChart.bind(this);
 
-		this.props.firebase.fs.collection('temp_sensor_test').orderBy('timestamp','desc').limit(20).get().then(query => {
-			query.forEach(doc => {
-				let timestamp = doc.data().timestamp
-				timestamp = new Date(timestamp * 1000);
-				let temperature = doc.data().temp
-				let tempdata = {x: timestamp, y: temperature}
-				dps.push(tempdata)
-			})
-		})
+		this.props.firebase.fs
+			.collection("temp_sensor_test")
+			.orderBy("timestamp", "desc")
+			.limit(20)
+			.get()
+			.then(query => {
+				query.forEach(doc => {
+					let timestamp = doc.data().timestamp;
+					timestamp = new Date(timestamp * 1000);
+					let temperature = doc.data().temp;
+					let tempdata = { x: timestamp, y: temperature };
+					dps.push(tempdata);
+				});
+			});
 	}
 
 	componentDidMount() {
-		this.props.firebase.fs.collection('temp_sensor_test').orderBy('timestamp','desc').limit(1).onSnapshot(snapshot => {
-			let changes = snapshot.docChanges();
-			changes.forEach(change => {
-				// this.setState({current: change.doc.data().temp})
-				let timestamp = change.doc.data().timestamp;
-				timestamp = new Date(timestamp * 1000);
-				let temperature = change.doc.data().temp
-				let tempdata = {x: timestamp, y: temperature}
-				dps.push(tempdata)
-				this.updateChart()
-			})
-		})
+		this.props.firebase.fs
+			.collection("temp_sensor_test")
+			.orderBy("timestamp", "desc")
+			.limit(1)
+			.onSnapshot(snapshot => {
+				let changes = snapshot.docChanges();
+				changes.forEach(change => {
+					// this.setState({current: change.doc.data().temp})
+					let timestamp = change.doc.data().timestamp;
+					timestamp = new Date(timestamp * 1000);
+					let temperature = change.doc.data().temp;
+					let tempdata = { x: timestamp, y: temperature };
+					dps.push(tempdata);
+					this.updateChart();
+				});
+			});
 	}
 
 	updateChart() {
 		// yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
 		// dps.push({x: xVal,y: yVal});
 		// xVal++;
-		if (dps.length > 20 ) {
+		if (dps.length > 20) {
 			dps.shift();
 		}
 		this.chart.render();
@@ -55,36 +64,41 @@ class DynamicLineChartBase extends Component {
 			// title :{
 			// 	text: "Temperature Line Chart"
 			// },
-			
-			axisY: { 
-				title: "Temperature (°C)", 
-			},    
-				
-			axisX: { 
-				title: "Time", 
+
+			axisY: {
+				title: "Temperature (°C)"
 			},
-			
-			data: [{
-				type: "line",
-				dataPoints : dps
-			}]
-		}
-		
+
+			axisX: {
+				title: "Time"
+			},
+
+			data: [
+				{
+					type: "line",
+					dataPoints: dps
+				}
+			]
+		};
+
 		return (
-		<div>
-			<br></br><br></br>
-			<Typography variant="h4" align="center">Temperature Dynamic Line Chart</Typography>
-			<br></br>
-			<CanvasJSChart options = {options} 
-				onRef={ref => this.chart = ref}
-			/>
-			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-		</div>
+			<div>
+				<br></br>
+				<br></br>
+				<Typography variant="h4" align="center">
+					Temperature Dynamic Line Chart
+				</Typography>
+				<br></br>
+				<CanvasJSChart
+					options={options}
+					onRef={ref => (this.chart = ref)}
+				/>
+				{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+			</div>
 		);
 	}
 }
 
 const DynamicLineChart = withRouter(withFirebase(DynamicLineChartBase));
-
 
 export default DynamicLineChart;
--- src/components/temperature.js
+++ src/components/temperature.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import '../App.css';
-import { withFirebase } from './Firebase';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import "../App.css";
+import { withFirebase } from "./Firebase";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 // import Grid from '@material-ui/core/Grid';
 // import Paper from '@material-ui/core/Paper';
 // import { positions } from '@material-ui/system';
@@ -17,68 +17,70 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
 const useStyles = makeStyles(theme => ({
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-    textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-        width: 200,
-    },
-    root: {
-        flexGrow: 1,
-        align:"center"
-    },
-    paper: {
-        padding: theme.spacing(2),
-        margin: 'auto',
-        maxWidth: 200,
-    },
-  }));
+	submit: {
+		margin: theme.spacing(3, 0, 2)
+	},
+	textField: {
+		marginLeft: theme.spacing(1),
+		marginRight: theme.spacing(1),
+		width: 200
+	},
+	root: {
+		flexGrow: 1,
+		align: "center"
+	},
+	paper: {
+		padding: theme.spacing(2),
+		margin: "auto",
+		maxWidth: 200
+	}
+}));
 
 const INITIAL_STATE = {
-    minTemp: 0, 
-    minTempDocID: '',
-    current: 0,
-    newMinTemp: ''
-};  
+	minTemp: 0,
+	minTempDocID: "",
+	current: 0,
+	newMinTemp: ""
+};
 
-class TemperatureDisplayBase extends Component{
+class TemperatureDisplayBase extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { ...INITIAL_STATE };
+		this.classes = { useStyles };
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = { ...INITIAL_STATE };
-        this.classes = { useStyles }
-        
-    }
+	componentDidMount() {
+		this.props.firebase.fs
+			.collection("device_settings")
+			.onSnapshot(snapshot => {
+				//to me, this is the 60 threshold.
+				let changes = snapshot.docChanges(); //if a new value is keyed in, update the value
+				changes.forEach(change => {
+					// both in the constant and in db.
+					if (change.doc.data().name === "temperature sensor") {
+						// this.state.minTemp = change.doc.data().minimum
+						this.setState({
+							minTemp: change.doc.data().minimum, //pulls from MINIMUM
+							minTempDocID: change.doc.id //the device settings doc id will change, so must update here for reference.
+						});
+					}
+				});
+			});
 
-    componentDidMount() {
-        this.props.firebase.fs.collection('device_settings')
-            .onSnapshot(snapshot => { //to me, this is the 60 threshold. 
-                let changes = snapshot.docChanges();                           //if a new value is keyed in, update the value 
-                changes.forEach(change => {                                     // both in the constant and in db. 
-                    if (change.doc.data().name === 'temperature sensor'){
-                        // this.state.minTemp = change.doc.data().minimum
-                        this.setState({
-                            minTemp: change.doc.data().minimum, //pulls from MINIMUM
-                            minTempDocID: change.doc.id      //the device settings doc id will change, so must update here for reference.
-                        })
-                    }
-                })
-            })
-        
-        this.props.firebase.fs.collection('temp_sensor_test')
-            .orderBy('timestamp','desc').limit(1)
-            .onSnapshot(snapshot => {
-                let changes = snapshot.docChanges();
-                changes.forEach(change => {
-                    this.setState({current: change.doc.data().temp}) //this is what the temp is. if i replace temp with timestamp then it changes
-                })
-            })
-        
-    }
+		this.props.firebase.fs
+			.collection("temp_sensor_test")
+			.orderBy("timestamp", "desc")
+			.limit(1)
+			.onSnapshot(snapshot => {
+				let changes = snapshot.docChanges();
+				changes.forEach(change => {
+					this.setState({ current: change.doc.data().temp }); //this is what the temp is. if i replace temp with timestamp then it changes
+				});
+			});
+	}
 
-    handleClickOpen = () => {
+	handleClickOpen = () => {
 		this.setState({
 			open: true
 		});
@@ -86,94 +88,100 @@ class TemperatureDisplayBase extends Component{
 
 	handleClose = () => {
 		this.setState({
-			open: false,
+			open: false
 		});
-    };
-    
-    onSubmit = event => {
-        event.preventDefault();
-        let num = Number(this.state.newMinTemp); //sets the var
-        // let num = this.newMinTemp;
-        console.log(num)
-        console.log(typeof num)
-        this.props.firebase.fs.collection('device_settings')
-            .doc(this.state.minTempDocID)
-            .update({ 
-                minimum: num 
-            }); //UPDATE FIRESTORE
+	};
 
-        this.handleClickOpen()
-        this.setState({newMinTemp: ''}) 
+	onSubmit = event => {
+		event.preventDefault();
+		let num = Number(this.state.newMinTemp); //sets the var
+		// let num = this.newMinTemp;
+		console.log(num);
+		console.log(typeof num);
+		this.props.firebase.fs
+			.collection("device_settings")
+			.doc(this.state.minTempDocID)
+			.update({
+				minimum: num
+			}); //UPDATE FIRESTORE
 
-    }
+		this.handleClickOpen();
+		this.setState({ newMinTemp: "" });
+	};
 
-    onChange = event => {
-        this.setState({
-            [event.target.name]: event.target.value 
-        });
-    }
+	onChange = event => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
 
-    render() {
-        const {newMinTemp} = this.state; 
+	render() {
+		const { newMinTemp } = this.state;
 
-        return ( 
-            <div align="center" className={this.classes.root}>
-            {/* <Paper className={this.classes.paper}>        */}
-                <p>Temperature Threshold: {this.state.minTemp}</p>
-                <p>Current Temperature: {this.state.current}</p>
-                <form onSubmit={this.onSubmit}>
-                
-                <div style={{ alignSelf: 'center' }}>
-                    <TextField 
-                        name='newMinTemp'
-                        value={newMinTemp}
-                        onChange={this.onChange}
-                        label="Set Threshold"
-                        defaultValue="65"
-                        type="Number"
-                        variant="outlined"
-                        margin="dense"
-                        align="left"
-                    /></div>
+		return (
+			<div align="center" className={this.classes.root}>
+				{/* <Paper className={this.classes.paper}>        */}
+				<p>Temperature Threshold: {this.state.minTemp}</p>
+				<p>Current Temperature: {this.state.current}</p>
+				<form onSubmit={this.onSubmit}>
+					<div style={{ alignSelf: "center" }}>
+						<TextField
+							name="newMinTemp"
+							value={newMinTemp}
+							onChange={this.onChange}
+							label="Set Threshold"
+							defaultValue="65"
+							type="Number"
+							variant="outlined"
+							margin="dense"
+							align="left"
+						/>
+					</div>
 
-                    <Button style={{ alignSelf: 'center' }}
-                        // disabled={isInvalid} 
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        className={this.classes.submit}
-                        margin="normal">
-                        Submit
-                    </Button>
-            
-                    <Dialog
-							open={this.state.open}
-							onClose={this.handleClose}
-							aria-labelledby="alert-dialog-title"
-							aria-describedby="alert-dialog-description"
-						>
-							<DialogTitle id="alert-dialog-title">
-								{"Submission Notification"}
-							</DialogTitle>
-							<DialogContent dividers>
-								<DialogContentText id="alert-dialog-description">
-									Temperature has been changed to {this.state.minTemp}
-								</DialogContentText>
-							</DialogContent>
-							<DialogActions>
-								<Button onClick={this.handleClose} color="primary" autoFocus>
-									Confirm
-								</Button>
-							</DialogActions>
-						</Dialog>
+					<Button
+						style={{ alignSelf: "center" }}
+						// disabled={isInvalid}
+						type="submit"
+						variant="contained"
+						color="primary"
+						className={this.classes.submit}
+						margin="normal"
+					>
+						Submit
+					</Button>
 
-                </form>
-                {/* </Paper>   */}
-            </div>
-        );
-    }
+					<Dialog
+						open={this.state.open}
+						onClose={this.handleClose}
+						aria-labelledby="alert-dialog-title"
+						aria-describedby="alert-dialog-description"
+					>
+						<DialogTitle id="alert-dialog-title">
+							{"Submission Notification"}
+						</DialogTitle>
+						<DialogContent dividers>
+							<DialogContentText id="alert-dialog-description">
+								Temperature has been changed to{" "}
+								{this.state.minTemp}
+							</DialogContentText>
+						</DialogContent>
+						<DialogActions>
+							<Button
+								onClick={this.handleClose}
+								color="primary"
+								autoFocus
+							>
+								Confirm
+							</Button>
+						</DialogActions>
+					</Dialog>
+				</form>
+				{/* </Paper>   */}
+			</div>
+		);
+	}
 }
 
 const TemperatureDisplay = withRouter(withFirebase(TemperatureDisplayBase));
 
-export default TemperatureDisplay
+export default TemperatureDisplay;
