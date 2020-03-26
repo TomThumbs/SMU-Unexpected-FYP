8e40e862bc14be10612700aad1f3c6c8cf3915a8
James <jamesteoh0104@gmail.com>
2020-03-26 15:01:52 +0800

    Minor bug fix
    ----- Post SOP -----
    fix minor issue when link from previous page

--- src/components/OrderPreparation/postsop.js
+++ src/components/OrderPreparation/postsop.js
@@ -94,7 +94,7 @@ class OrderPreparationPostSopBase extends Component {
 								pathname: ROUTES.ORDER_TIMELINE,
 								search: "?id=" + this.props.location.orderID,
 								state: {
-									orderID: this.state.orderID
+									orderID: this.props.location.orderID
 								}
 							}}>Back to Timeline
 							</Button>
